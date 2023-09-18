e8e4b1c483b596c6db54b81fd01d2e43228c5a85
Resn4R <purview-joust.0x@icloud.com>
2023-09-18 11:04:04 +0100

    Initial Commit

---
+++ color circles/color_circlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  color_circlesApp.swift
+//  color circles
+//
+//  Created by Vito Borghi on 18/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct color_circlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
