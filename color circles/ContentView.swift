1793b1172df886c2be183dd18b0f5fbcc3daa3b8
Resn4R <purview-joust.0x@icloud.com>
2023-09-19 12:44:01 +0100

    added accelerometer support, buttons to change between gyro and accelerometer

--- color circles/ContentView.swift
+++ color circles/ContentView.swift
@@ -11,67 +11,98 @@ import CoreMotion
 
 
 struct ContentView: View {
-    @State private var amount = 0.0
+    enum motionTypes {
+        case gyro, accelerometer
+    }
     @State private var manager = CMMotionManager()
-    @State private var gyroX: Double?
-    @State private var gyroY: Double?
-    @State private var gyroZ: Double?
+    @State private var motionX: Double?
+    @State private var motionY: Double?
+    @State private var motionZ: Double?
     
-    func startGyro(){
-        print(manager.isGyroAvailable)
-        print(manager.isGyroActive)
+    func startMotion(with motionType: motionTypes){
         
-        if manager.isGyroAvailable {
-            manager.gyroUpdateInterval = 0.0001
+        if motionType == .gyro {
+            manager.stopAccelerometerUpdates()
             
             manager.startGyroUpdates(to: .main) { (data, error) in
-                gyroX = abs((manager.gyroData?.rotationRate.x)!)
-                gyroY = abs((manager.gyroData?.rotationRate.y)!)
-                gyroZ = abs((manager.gyroData?.rotationRate.z)!)
+                motionX = abs((manager.gyroData?.rotationRate.x)!)
+                motionY = abs((manager.gyroData?.rotationRate.y)!)
+                motionZ = abs((manager.gyroData?.rotationRate.z)!)
             }
         } else {
-            fatalError("Gyroscope not available on this device.")
-        }
-    }
-    
-    var body: some View {
-        VStack{
-            ZStack{
-                Circle()
-                    .fill(Color(red: 1, green: 0, blue: 0))
-                    .frame(width: 200 * (gyroX ?? amount))
-                    .offset(x: -50, y: -80)
-                    .blendMode(.screen)
-                
-                Circle()
-                    .fill(Color(red: 0, green: 1, blue: 0))
-                    .frame(width: 200 * (gyroY ?? amount))
-                    .offset(x: 50, y: -80)
-                    .blendMode(.screen)
-                
-                Circle()
-                    .fill(Color(red: 0, green: 0, blue: 1))
-                    .frame(width: 200 * (gyroZ ?? amount))
-                    .blendMode(.screen)
-            }
-            .frame(width: 300, height: 300)
+            manager.stopGyroUpdates()
             
-            Slider(value: $amount)
-                .padding()
+            manager.startAccelerometerUpdates(to: .main) { (data, error) in
+                motionX = abs((manager.accelerometerData?.acceleration.x)!)
+                motionY = abs((manager.accelerometerData?.acceleration.y)!)
+                motionZ = abs((manager.accelerometerData?.acceleration.z)!)
+            }
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(.black)
-        .ignoresSafeArea()
+    }
         
-        .onAppear{
-            startGyro()
+        func canYouRunIt() {
+            guard manager.isGyroAvailable else {
+                fatalError("Gyroscope not available on this device.")
+            }
+            print("Gyro available")
+            
+            guard manager.isAccelerometerAvailable else {
+                fatalError("Accelerometer not available on this device.")
+            }
+            print("Accelerometer available")
         }
+        
+        var body: some View {
+            VStack{
+                ZStack{
+                    Circle()
+                        .fill(Color(red: 1, green: 0, blue: 0))
+                        .frame(width: 200 * (motionX ?? 0))
+                        .offset(x: -50, y: -80)
+                        .blendMode(.screen)
+                    
+                    Circle()
+                        .fill(Color(red: 0, green: 1, blue: 0))
+                        .frame(width: 200 * (motionY ?? 0))
+                        .offset(x: 50, y: -80)
+                        .blendMode(.screen)
+                    
+                    Circle()
+                        .fill(Color(red: 0, green: 0, blue: 1))
+                        .frame(width: 200 * (motionZ ?? 0))
+                        .blendMode(.screen)
+                }
+                .frame(width: 300, height: 300)
+                
+                
+                VStack{
+                    Group{
+                        Button("Gyroscope"){
+                            startMotion(with: .gyro)
+                        }
+                        .padding()
+                        Button("Accelerometer"){
+                            startMotion(with: .accelerometer)
+                        }
+                    }
+                    .buttonStyle(.borderedProminent)
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.black)
+            .ignoresSafeArea()
+            .onAppear{
+                canYouRunIt()
+                
+                manager.gyroUpdateInterval = 0.0001
+                manager.accelerometerUpdateInterval = 0.0001
+            }
     }
 }
     
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
-}
